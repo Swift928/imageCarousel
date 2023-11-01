a91659f31e6309597595b9b9db6bb31bee903a91
Keith Taylor <68087065+Swift928@users.noreply.github.com>
2023-11-01 10:23:37 -0500

    finished making it functional

--- script.js
+++ script.js
@@ -1,27 +1,57 @@
 let slideButton = document.querySelectorAll('.arrowButton');
+let slides = document.querySelectorAll('.slide');
 
 slideButton.forEach((item) => {
     item.addEventListener('click', () => {
-        let slides = document.querySelectorAll('.slide');
-        slides.forEach((image, index) => {
-            if (image.hasAttribute('data-active')) {
-                image.removeAttribute('data-active');
-                if (item.classList.contains('right')) {
-                    if (index === slides.length - 1) {
-                        slides[0].setAttribute('data-active', '');
-                    } else {
-                        slides[index + 1].setAttribute('data-active', '');
-                    }
-                } else {
-                    if (index === 0) {
-                        slides[slides.length - 1].setAttribute(
-                            'data-active',
-                            ''
-                        );
-                    } else {
-                        slides[index - 1].setAttribute('data-active', '');
-                    }
-                }
+        let currentSlide = document.querySelector('.slide[data-active="true"]');
+        if (item.classList.contains('rightArrowButton')) {
+            if (currentSlide) {
+                currentSlide.removeAttribute('data-active');
+                let nextSlide = currentSlide.nextElementSibling || slides[0];
+                nextSlide.setAttribute('data-active', 'true');
+            }
+        } else {
+            if (currentSlide) {
+                currentSlide.removeAttribute('data-active');
+                let nextSlide =
+                    currentSlide.previousElementSibling ||
+                    slides[slides.length - 1];
+                nextSlide.setAttribute('data-active', 'true');
+            }
+        }
+    });
+});
+
+let displayDots = document.querySelector('.displayDots');
+let svg = document.querySelector('.imageDot');
+
+slides.forEach(() => {
+    let copiedDot = svg.cloneNode(true);
+    displayDots.appendChild(copiedDot);
+    displayDots.firstElementChild.setAttribute('data-active', 'true');
+});
+
+let dots = document.querySelectorAll('.imageDot');
+dots.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        slides.forEach((item) => item.removeAttribute('data-active'));
+        slides[index].setAttribute('data-active', 'true');
+        dots.forEach((item) => item.removeAttribute('data-active'));
+        item.setAttribute('data-active', 'true');
+    });
+});
+
+function updateDots(index) {
+    dots.forEach((item) => item.removeAttribute('data-active'));
+    dots[index].setAttribute('data-active', 'true');
+}
+
+slideButton.forEach((item) => {
+    item.addEventListener('click', () => {
+        slides.forEach((item, index) => {
+            if (item.hasAttribute('data-active')) {
+                dots.forEach((item) => item.removeAttribute('data-active'));
+                dots[index].setAttribute('data-active', 'true');
             }
         });
     });
